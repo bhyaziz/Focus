eb73d13e5ef956566a6062cae9d49a64f926d2cb
Mohamed Aziz Ben Haj Yahia <azizbhy2004@gmail.com>
2025-04-09 14:09:48 +0100

    Update work.js

--- js/work.js
+++ js/work.js
@@ -4,8 +4,8 @@ const timers = {} // Object to store all timers
 const projectTasks = {} // Object to store tasks for each project
 const projects=[];
 try {
-  const PARSE_APPLICATION_ID = "LmeCejjiVPk4sPBTz4ldeFuRhuDNvnIrvtFSgoS1"
-  const PARSE_JAVASCRIPT_KEY = "Op9NBG0V08Sw8K7tD4CzUw1fN0cokBmNeCwEs9Hp"
+  const PARSE_APPLICATION_ID = "CJJFInfLC3tjEyyvkMW9ueDfrfcQrI8pZFMrwWnh"
+  const PARSE_JAVASCRIPT_KEY = "xgUeoU2FNVCTAqzWHAXrsc6JENKxotOoRFSrCs4R"
   const PARSE_SERVER_URL = "https://parseapi.back4app.com/"
 
   Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY)
